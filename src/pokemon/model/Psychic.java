e6bbae5436486282f9f3c703561d3928fa85c8af
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-12-18 12:33:35 -0700

    Added Psychic type

---
+++ src/pokemon/model/Psychic.java
@@ -0,0 +1,9 @@
+package pokemon.model;
+
+public interface Psychic {
+	public String waitForIt(int damage);
+	public String mindReader(int damage);
+	public String randomQuote(int damage);
+	public String pineapple(int damage);
+	
+}
