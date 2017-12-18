eee6f65c9b603ece246f5e0d4e0403bea5a2b3c9
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-12-18 12:33:45 -0700

    Added Rock type

---
+++ src/pokemon/model/Rock.java
@@ -0,0 +1,9 @@
+package pokemon.model;
+
+public interface Rock {
+	public String armCrush(int damage);
+	public String rockSmash(int damage);
+	public String rockBottom(int damage);
+	public String sizeUp(int damage);
+	
+}
