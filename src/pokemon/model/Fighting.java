5bc722a846057f0d82de71b71844f0fb283358ae
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-12-18 12:33:20 -0700

    Added Fighting type

---
+++ src/pokemon/model/Fighting.java
@@ -0,0 +1,9 @@
+package pokemon.model;
+
+public interface Fighting {
+	public String roundhouseKick(int damage);
+	public String rapidFire(int damage);
+	public boolean intimidate(int damage);
+	public boolean shearAwesomeness(int damage);
+	
+}
