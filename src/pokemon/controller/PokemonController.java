a4f8599403760def5011d4a42161550d0d59a5ea
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-01-08 12:03:28 -0700

    Added Convert Pokedex

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,7 +1,31 @@
 package pokemon.controller;
 
+import pokemon.model.*;
+import java.util.*;
+
 public class PokemonController {
+	private List<Pokemon> pokedex;
+	
 	public void start() {
+//		setupPokemon();
+	}
+//	public List<Pokemon> setupPokemon() {
 		
+//	}
+	public List<Pokemon> getPokedex() {
+		return pokedex;
+	}
+	public boolean isValidInteger(String input) {
+		return false;
+	}
+	public boolean isValidDouble(String input) {
+		return false;
+	}
+	public String [] convertPokedex() {
+		String [] names = new String [pokedex.size()];
+		for (int i = 0; i < pokedex.size(); i++) {
+			names[i] = pokedex.get(i).getName();
+		}
+		return names;
 	}
 }
