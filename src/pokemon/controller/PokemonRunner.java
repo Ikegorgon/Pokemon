d2a205fa9f7772ab1af9660e37f5005b82738cd0
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-01-10 12:24:26 -0700

    Removed start method

--- src/pokemon/controller/PokemonRunner.java
+++ src/pokemon/controller/PokemonRunner.java
@@ -3,6 +3,5 @@ package pokemon.controller;
 public class PokemonRunner {
 	public static void main (String [ ] args) {
 		PokemonController PokemonApp = new PokemonController();
-		PokemonApp.start();
 	}
 }
