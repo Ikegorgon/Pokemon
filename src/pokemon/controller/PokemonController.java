29b4d6603fcf7aaa1e90baddcd9d5f6547dbd255
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-01-10 12:24:55 -0700

    Removed Start Method and Added new Code

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,13 +1,24 @@
 package pokemon.controller;
 
 import pokemon.model.*;
+import pokemon.view.*;
 import java.util.*;
 
 public class PokemonController {
 	private List<Pokemon> pokedex;
+	private PokemonFrame appFrame;
 	
-	public void start() {
-//		setupPokemon();
+	public PokemonController() {
+		pokedex = new ArrayList<Pokemon>();
+		pokedex.add(new Geodude());
+		pokedex.add(new Graveler());
+		pokedex.add(new Golem());
+		pokedex.add(new Wobbuffet());
+		pokedex.add(new Terrakion());
+		pokedex.add(new TheRock());
+		pokedex.add(new ShawnSpencer());
+		pokedex.add(new ChuckNorris());
+		appFrame = new PokemonFrame(this);
 	}
 //	public List<Pokemon> setupPokemon() {
 		
